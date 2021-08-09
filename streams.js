8981e6ff19efa09608b94ab2f7f57ed03457cb23
DizzyReading <daintybeat@gmail.com>
2021-08-09 21:55:44 +0530

    Node.js Basics
    Piping

--- streams.js
+++ streams.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8' }); // turns to readable format
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-readStream.on('data', (chunk) => {
-	console.log('--- NEW CHUNK ---');
-	console.log(chunk);
-	writeStream.write('\nNEW CHUNK\n');
-	writeStream.write(chunk);
-});
+//piping
+
+readStream.pipe(writeStream); //same process but shorter
