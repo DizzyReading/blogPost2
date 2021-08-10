394d9c75fbf76989c8caf2b1dd681fc443d2507f
DizzyReading <daintybeat@gmail.com>
2021-08-10 20:48:26 +0530

    Express Apps
    routing & sending html pages

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ which we are storing in a var*/
 app.listen(5000);
 
 app.get('/', (req, res) => {
-	res.send(
-		'<p>Home Page</p>'
-	); /* <-- this automatically infers the TYPE of CONTENT 
-  we are trying to send the browser and sets the Headers Content Type.
-  Also, it also infers the STATUS CODE
+	// res.send(
+	// 	'<p>Home Page</p>'
+	// );
 
-  */
+	res.sendFile('./views/index.html', {
+		root: __dirname
+	}); /* <-- we add a second argument (an opitons object) because
+  by default express will look for an absolute path from the root of our computer. 
+  This will have root as its property to specify what the './views/index.html' (first arg) 
+  is relative to */
+});
+
+app.get('/about', (req, res) => {
+	// res.send('<p>About Page</p>');
+	res.sendFile('./views/about.html', { root: __dirname });
 });
