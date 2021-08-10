f4c39f3ec8990742620c510db8ab0dca763225ff
DizzyReading <daintybeat@gmail.com>
2021-08-10 16:20:35 +0530

    Requests & Responses
    Res Object

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const server = http.createServer((req, res) => {
   becuase we wanna send back a diff res (resposes) based on
   that route
   */
+
+	// set header content type
+
+	res.setHeader('Content-Type', 'text/html');
+
+	res.write(
+		'<head><link rel="stylesheet" href="#"></head>'
+	); /* we can create our own html tags and 
+  effectively replace the default one in the browser
+  */
+
+	res.write('<p>Hole to Another Universe</p>');
+	res.write('<p>Arcadia Bay</p>');
+
+	res.end();
 });
 
 server.listen(5000, 'localhost', () => {
