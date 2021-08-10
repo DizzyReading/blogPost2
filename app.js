145281e70d08ebc100896a0cde04c2c01b9093e8
DizzyReading <daintybeat@gmail.com>
2021-08-10 21:09:29 +0530

    Express Apps
    redirecting & 404 pages

--- app.js
+++ app.js
@@ -27,3 +27,27 @@ app.get('/about', (req, res) => {
 	// res.send('<p>About Page</p>');
 	res.sendFile('./views/about.html', { root: __dirname });
 });
+
+// redirects
+
+app.get('/about-us', (req, res) => {
+	res.redirect(
+		'/about'
+	); /* <-- under the hood express sends this res(ponse) 
+  to the browser & forces it into a new request for '/about'
+  */
+});
+
+// 404 page
+
+app.use((req, res) => {
+	/* <-- use this function for every incoming req regardless of url.
+  Basically telling express if we dont get a match for any of the urls than respond 
+  with a 404 page. Hence, it must be at the bottom of the file.
+
+  Additionally, express as of now wont realize that this is some kind of error and
+  wont change the STATUS CODE to 404. We will have to se that manually.
+
+  */
+	res.status(404).sendFile('/views/404.html', { root: __dirname });
+});
