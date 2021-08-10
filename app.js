1a710306f499c28591b08f499dd4ffffec0e3306
DizzyReading <daintybeat@gmail.com>
2021-08-10 22:13:42 +0530

    View Engines
    use ejs view engine

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
 const express = require('express'); // <-- returns a function
 
-//express app
+// express app
 
 const app = express(); /*<-- invoking that function 
 to create an instance of an express app
 which we are storing in a var*/
+
+// register view engine
+
+app.set('view engine', 'ejs'); /* app.set() lets us configure app settings */
 
 // listen for requests
 
 app.listen(5000);
 
 app.get('/', (req, res) => {
-	// res.send(
-	// 	'<p>Home Page</p>'
-	// );
-
-	res.sendFile('./views/index.html', {
-		root: __dirname
-	}); /* <-- we add a second argument (an opitons object) because
-  by default express will look for an absolute path from the root of our computer. 
-  This will have root as its property to specify what the './views/index.html' (first arg) 
-  is relative to */
+	res.render('index');
 });
 
 app.get('/about', (req, res) => {
-	// res.send('<p>About Page</p>');
-	res.sendFile('./views/about.html', { root: __dirname });
+	res.render('about');
 });
 
-// redirects
-
-app.get('/about-us', (req, res) => {
-	res.redirect(
-		'/about'
-	); /* <-- under the hood express sends this res(ponse) 
-  to the browser & forces it into a new request for '/about'
-  */
+app.get('/blogs/create', (req, res) => {
+	res.render('create');
 });
 
 // 404 page
 
 app.use((req, res) => {
-	/* <-- use this function for every incoming req regardless of url.
-  Basically telling express if we dont get a match for any of the urls than respond 
-  with a 404 page. Hence, it must be at the bottom of the file.
-
-  Additionally, express as of now wont realize that this is some kind of error and
-  wont change the STATUS CODE to 404. We will have to se that manually.
-
-  */
-	res.status(404).sendFile('/views/404.html', { root: __dirname });
+	res.status(404).render('404');
 });
