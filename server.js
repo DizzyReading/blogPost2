07fe32526c71de4261597b0ac625fb4c1b2d4bfc
DizzyReading <daintybeat@gmail.com>
2021-08-10 16:05:44 +0530

    Requests & Responses
    Req Obj

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 	console.log('request made');
+	console.log(req.url, req.method);
+	/* info about url is gonna be very useful later 
+  becuase we wanna send back a diff res (resposes) based on
+  that route
+  */
 });
 
 server.listen(5000, 'localhost', () => {
