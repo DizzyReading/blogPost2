b416c29d97378ba7413577ebff4ffd8303ca3496
DizzyReading <daintybeat@gmail.com>
2021-08-11 13:05:24 +0530

    View Engines
    Pass data into views

--- app.js
+++ app.js
@@ -15,15 +15,20 @@ app.set('view engine', 'ejs'); /* app.set() lets us configure app settings */
 app.listen(5000);
 
 app.get('/', (req, res) => {
-	res.render('index');
+	const blogs = [
+		{ title: 'alt-J', snippet: 'English indie rock band' },
+		{ title: 'Local Natives', snippet: 'American Indie rock band' },
+		{ title: 'Syd Matters', snippet: 'French indie band' }
+	];
+	res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-	res.render('about');
+	res.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (req, res) => {
-	res.render('create');
+	res.render('create', { title: 'Create' });
 });
 
 // 404 page
